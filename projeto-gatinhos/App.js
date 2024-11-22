42b0f1a62ac3a6b2dc9e262c37c3b79b185231b3
João Vitor Serra de Freitas <joao.freitas29@fatec.sp.gov.br>
2024-11-22 16:55:48 -0300

    docs(p2-pdm): cria api para buscar fotos

--- projeto-gatinhos/App.js
+++ projeto-gatinhos/App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios  from 'axios';
 
 export default function App() {
+
+  const getPhotos = async () => {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=5');
+      return response.data;
+    } catch (error) {
+      console.error('Ocorreu um erro ao buscar as fotos.', error);
+      return [];
+    }
+  };
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
